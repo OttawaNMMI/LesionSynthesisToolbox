3c70d19e9228a0af17fb2866efc85faf1b584057
Ran Klein <rklein@toh.ca>
2022-11-28 20:11:35 -0500

    Fixed status indicator timer cleanup on closing application

--- Lesion_Synthesis_Toolbox_App/Supporting_Code/refreshLSTProcessingServiceIndicatorStatus.m
+++ Lesion_Synthesis_Toolbox_App/Supporting_Code/refreshLSTProcessingServiceIndicatorStatus.m
@@ -1,8 +1,13 @@
 function refreshLSTProcessingServiceIndicatorStatus(hObject, event)
 obj = get(hObject,'UserData');
-if strcmpi(LSTProcessingService('status'),'Running')
-	set(obj,'Color','Green');
-else
-	set(obj,'Color','Red');
+if isvalid(obj)
+	if strcmpi(LSTProcessingService('status'),'Running')
+		set(obj,'Color','Green');
+	else
+		set(obj,'Color','Red');
+	end
+else % The timer to check for status is running, but the indicator does not exist anymore. No point running this time any longer.
+	stop(hObject);
+	delete(hObject);
 end
 end
